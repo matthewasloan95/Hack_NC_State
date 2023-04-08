f09da0f79cace373a1e280eefbf08dda883e74fa
rishabhravi1 <rravi3@uncc.edu>
2023-04-08 12:44:48 -0400

    added player class

---
+++ TA-Hack-State/JS/index.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+const player = new Player
+
+//let bottom = y + 100
+function animate(){
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.draw()
+    player.update()
+}
+
+animate()
